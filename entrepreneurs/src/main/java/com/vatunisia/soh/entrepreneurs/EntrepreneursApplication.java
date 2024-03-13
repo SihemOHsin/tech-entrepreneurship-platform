7f2c21027619c6a96df47685892666160945b8e2
SihemOuledHsin <91225008+SihemOHsin@users.noreply.github.com>
2024-03-13 22:39:25 +0100

    add entrepreneur microservice

---
+++ entrepreneurs/src/main/java/com/vatunisia/soh/entrepreneurs/EntrepreneursApplication.java
@@ -0,0 +1,13 @@
+package com.vatunisia.soh.entrepreneurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrepreneursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrepreneursApplication.class, args);
+	}
+
+}
---
+++ entrepreneurs/src/test/java/com/vatunisia/soh/entrepreneurs/EntrepreneursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vatunisia.soh.entrepreneurs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrepreneursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
